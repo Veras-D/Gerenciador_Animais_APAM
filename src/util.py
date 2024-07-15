7b50b3a83a033be3175504a0da9f8e5d0159fee6
Veras-D <dveras2310@gmail.com>
2024-07-15 04:48:43 -0300

    Update util.py

--- src/util.py
+++ src/util.py
@@ -1,4 +1,74 @@
-class Util:
+import re
+import sqlite3
+
+class Validacao:
     def __init__(self):
-        pass
+        self.reg_field = r'[\s\-_,\"\'\*\.\!\?\;\:\(\)\[\]\{\}\<\>\|\\\/]'
+        self.reg_email = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+
+    @staticmethod
+    def tratar_dados(self, dado):
+        return re.sub(selgf.reg, "", dado)
+
+    @staticmethod
+    def tratarCPF(cpf):
+        return Validacao.tratar_dados(cpf)
+
+    @staticmethod
+    def tratarRG(rg):
+        return Validacao.tratar_dados(rg)
+
+    @staticmethod
+    def tratarCEP(cep):
+        return Validacao.tratar_dados(cep)
+
+    @staticmethod
+    def tratarTelefone_e_Celular(telefone):
+        return Validacao.tratar_dados(telefone)
+
+    @staticmethod
+    def validarCPF(cpf):
+        cpf = Validacao.tratarCPF(cpf)
+        if len(cpf) != 11 or not cpf.isdigit():
+            return False
+        return True
+
+    @staticmethod
+    def validarRG(rg):
+        rg = Validacao.tratarRG(rg)
+        if len(rg) < 7 or len(rg) > 14 or not rg.isdigit():
+            return False
+        return True
+
+    @staticmethod
+    def validarCEP(cep):
+        cep = Validacao.tratarCEP(cep)
+        if len(cep) != 8 or not cep.isdigit():
+            return False
+        return True
+
+    @staticmethod
+    def validarNome(nome):
+        if not nome or not nome.replace(" ", "").isalpha():
+            return False
+        return True
+
+    @staticmethod
+    def validarEmail(self, email):
+        return re.match(self.reg_email, email) is not None
+
+    @staticmethod
+    def validarTelefone_e_Celular(telefone):
+        telefone = Validacao.tratarTelefone_e_Celular(telefone)
+        if len(telefone) not in [10, 11] or not telefone.isdigit():
+            return False
+        return True
+
+    @staticmethod
+    def validarIdade(idade):
+        return idade.isdigit() and 0 <= int(idade) <= 120
+
+    @staticmethod
+    def verificarCampo(campo):
+        return campo.strip() != ""
 
---
+++ tests/util/test_util.py
@@ -0,0 +1,9 @@
+import pytest
+from src.util import Validacao
+
+
+def test_tratar_dados():
+    pass
+
+
+validacao = Validacao()
