902481aa9b0b41502d9def4189ca21b5dfa4c913
Veras-D <dveras2310@gmail.com>
2024-07-17 11:41:47 -0300

    Fix test_util.py import

---
+++ tests/Util/test_util.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+
+from util import Validacao
+
+
+def test_tratar_dados():
+    pass
+
+
+validacao = Validacao()
--- tests/util/test_util.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from src.util import Validacao
-
-
-def test_tratar_dados():
-    pass
-
-
-validacao = Validacao()
