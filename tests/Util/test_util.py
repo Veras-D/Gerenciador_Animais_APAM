1f78094cf670db9498c11065e504917530fbf068
Veras-D <dveras2310@gmail.com>
2024-07-17 16:53:13 -0300

    Update Conceptual Model

--- tests/Util/test_util.py
+++ tests/Util/test_util.py
@@ -1,7 +1,7 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+if __name__ == "__main__":
+    import sys
+    import os
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
 
 from util import Validacao
 
