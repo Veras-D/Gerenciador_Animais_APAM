ceae822564cd386c8de143be987af47c8470013c
Lucas Alexandre <67240088+Lucas-Alexandre62@users.noreply.github.com>
2024-07-17 13:44:01 -0300

    Update test_util.py

--- tests/Util/test_util.py
+++ tests/Util/test_util.py
@@ -7,7 +7,50 @@ from util import Validacao
 
 
 def test_tratar_dados():
-    pass
+    assert Validacao.tratar_dados
 
+def test_tratarCPF():
+    assert Validacao.tratarCPF
 
-validacao = Validacao()
+def test_tratarRG():
+    assert Validacao.tratarRG
+
+def test_tratarCEP():
+    assert Validacao.tratarCEP
+
+def test_tratarTelefone_e_Celular():
+    assert Validacao.tratarTelefone_e_Celular
+
+def test_validarCPF():
+    assert Validacao.validarCPF
+
+def test_validarRG():
+    assert Validacao.validarRG
+
+def test_validarCEP():
+    assert Validacao.validarCEP
+
+def test_validarEmail():
+    assert Validacao.validarEmail
+
+def test_validarTelefone_e_Celular():
+    assert Validacao.validarTelefone_e_Celular
+
+def test_validarIdade():
+    assert Validacao.validarIdade
+
+def test_verificarCampo():
+    assert Validacao.verificarCampo
+
+test_tratar_dados()
+test_tratarCPF()
+test_tratarRG()
+test_tratarCEP()
+test_tratarTelefone_e_Celular()
+test_validarCPF()
+test_validarRG()
+test_validarCEP()
+test_validarEmail()
+test_validarTelefone_e_Celular()
+test_validarIdade()
+test_verificarCampo()
