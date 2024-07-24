ca351ff65871fb092a53703837ad90e24655684a
Veras-D <dveras2310@gmail.com>
2024-07-23 23:54:08 -0300

    add entity adotante

--- src/Models/Entities/adotante.py
+++ src/Models/Entities/adotante.py
@@ -4,3 +4,33 @@ from dataclasses import dataclass
 @dataclass
 class Adortante:
     nome: str
+    rg: str
+    cpf: str
+    rua: str
+    numero: str
+    complemento: str: 'Não Informado'
+    bairro: str
+    cep: str
+    cidade: str
+    estado: str
+    profissao: str = 'Não Informado'
+    email: str 
+    telefone_fixo: str = 'Não Informado'
+    telefone_celular: str
+    referencia_rua: str
+
+
+
+@dataclass
+class ObservacaoAdotante:
+    id_adotante: int
+    observacao: str
+    data_observacao: str
+
+
+@dataclass
+class AdocaoAdotante:
+    id_info_animal: int
+    id_dotante: int
+    data_adocao: str
+    
